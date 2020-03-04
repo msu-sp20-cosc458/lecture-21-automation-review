5536ca839a7e2edbe082f2cbcf152388c4adeee1
Phil B <philanab@gmail.com>
2020-03-04 10:50:41 -0500

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,11 +4,11 @@ import functions
 class ChatBotResponseTest(unittest.TestCase):
     def test_not_command(self):
         response = functions.get_chatbot_response("Purple")
-        self.assertEquals(response, "")
+        self.assertEqual(response, "")
         
     def test_add_command(self):
         response = functions.get_chatbot_response(''' put something here ''')
-        self.assertEquals(''' put something here''')
+        self.assertEqual(''' put something here''')
 
 if __name__ == '__main__':
     unittest.main()
