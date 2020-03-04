23eb6f7e6b6e5af7e059e95166d17b6a5399aa8c
Phil B <philanab@gmail.com>
2020-03-04 09:28:36 -0500

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,8 +7,8 @@ class ChatBotResponseTest(unittest.TestCase):
         self.assertEquals(response, "")
         
     def test_add_command(self):
-        response = functions.get_chatbot_response("!! add 2 2")
-        self.assertEquals(response, 4)
+        response = functions.get_chatbot_response(''' put something here ''')
+        self.assertEquals(''' put something here''')
 
 if __name__ == '__main__':
     unittest.main()
