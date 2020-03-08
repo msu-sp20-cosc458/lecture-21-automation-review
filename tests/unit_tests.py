d20741901bd435c7967371977a686c78cfd974ac
Ubuntu <ubuntu@ip-172-31-17-71.ec2.internal>
2020-03-08 23:12:20 +0000

    fixed issue with our tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,10 +5,11 @@ class ChatBotResponseTest(unittest.TestCase):
     def test_not_command(self):
         response = functions.get_chatbot_response("Purple")
         self.assertEqual(response, "")
+        self.assertEqual(response, "")
         
     def test_add_command(self):
-        response = functions.get_chatbot_response(''' put something here ''')
-        self.assertEqual(''' put something here''')
+        response = functions.get_chatbot_response("!! add 4 2")
+        self.assertEqual(response, 6)
 
 if __name__ == '__main__':
     unittest.main()
